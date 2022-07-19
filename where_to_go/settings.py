98ceb915daf11397176a9cfefc2a0dfa2ce69598
Vitaly Borodulin <rainbowf@mail.ru>
2022-07-19 20:59:41 +0500

    Set different directories for STATIC_ROOT and STATICFILES_DIRS

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
-if DEBUG:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-else:
-    STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.getenv(
+    'STATIC_ROOT',
+    os.path.join(BASE_DIR, 'collected_static')
+)
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 MEDIA_ROOT = os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
