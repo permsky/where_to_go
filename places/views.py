45fb1fd176ed0c9918cc9fc5acff56d89c7f0325
Vitaly Borodulin <rainbowf@mail.ru>
2022-07-20 20:13:01 +0500

    Fix variable names

--- places/views.py
+++ places/views.py
@@ -5,21 +5,21 @@ from .models import Place
 
 
 def get_place(request, place_id):
-    place_obj = get_object_or_404(Place, id=int(place_id))
+    place = get_object_or_404(Place, id=int(place_id))
     images = list()
-    for img in place_obj.images.all().order_by('precedence'):
+    for img in place.images.all().order_by('precedence'):
         images.append(img.image.url)
-    place = {
-        'title': place_obj.title,
+    serialized_place = {
+        'title': place.title,
         'imgs': images,
-        'description_short': place_obj.description_short,
-        'description_long': place_obj.description_long,
+        'description_short': place.description_short,
+        'description_long': place.description_long,
         'coordinates': {
-            'lat': place_obj.latitude,
-            'lng': place_obj.longitude
+            'lat': place.latitude,
+            'lng': place.longitude
         }
     }
     return JsonResponse(
-        place,
+        serialized_place,
         json_dumps_params={'ensure_ascii': False, 'indent': 4}
     )
